cd67f98a32d3cab47f51f9867bfb5b19baa2cce2
jacobbustamante <jacobandrewbustamante@gmail.com>
2014-11-17 13:44:11 -0800

    refactored out package

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dungeoncrawler;
+
 
 /**
  *
  * @author jacobbustamante
  */
-public class Character {
+public class Dungeon {
    
 }
